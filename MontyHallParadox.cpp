8de1475074ad97d90fc70333c44e4d6a153ae1c1
TuckerSelf <tself3@ut.utm.edu>
2023-11-07 17:10:40 -0600

    functions

--- MontyHallParadox.cpp
+++ MontyHallParadox.cpp
@@ -22,14 +22,13 @@ int main() {
 }
 
 bool monty_hall(){
-  int playerChoice;
   char doors[3] = {'G', 'G', 'G'};
 
   int carDoor = rand()%3;
 
   doors[carDoor] = 'C';
 
-  int playerChoice = 0;
+  int playerChoice = 1;
   int removeDoor = 0;
   while(removeDoor == (playerChoice-1) || removeDoor == carDoor){
     removeDoor++;
@@ -40,4 +39,27 @@ bool monty_hall(){
     return true;
   else
     return false;
+}
+
+void simulate(int num_iterations){
+  int gamesPlayed = 0;
+  int stay = 0;
+  int change = 0;
+  bool carWin;
+  for(int i=0; i<num_iterations; i++){
+    carWin = monty_hall();
+    gamesPlayed++;
+    if(carWin == true)
+      stay++;
+    else
+      change++;
+  }
+
+  double stayPercentage = (stay/gamesPlayed)*10.00;
+  double changePercentage = (change/gamesPlayed)*10.00;
+  
+  cout << "Wins by Staying: " << stay << endl << "Wins by Switching: " << change << endl
+       << "Times Played: " << gamesPlayed << endl;
+  cout << "Win Percentage when Staying: " << stayPercentage << "%" << endl;
+  cout << "Win Percentage when Switching: " << changePercentage << "%" << endl;
 }
