b39d821db5f5036e69cef054df1b53c6841b0410
TuckerSelf <tself3@ut.utm.edu>
2023-11-07 16:27:48 -0600

    bool fix

--- MontyHallParadox.cpp
+++ MontyHallParadox.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-void monty_hall();
+bool monty_hall();
 
 void simulate(int num_iterations);
 
@@ -21,49 +21,23 @@ int main() {
   return 0;
 }
 
-void monty_hall(){
+bool monty_hall(){
   int playerChoice;
   char doors[3] = {'G', 'G', 'G'};
-  char closedDoors[3] = {'D', 'D', 'D'};
 
   int carDoor = rand()%3;
 
   doors[carDoor] = 'C';
 
-  for(int i = 0; i<3; i++)
-    cout << closedDoors[i] << " ";
-  cout << endl;
-  cout << "Choose a door (1 - 3): ";
-  cin >> playerChoice;
-  
+  int playerChoice = 0;
   int removeDoor = 0;
   while(removeDoor == (playerChoice-1) || removeDoor == carDoor){
     removeDoor++;
   }
   doors[removeDoor] = 'X';
-  closedDoors[removeDoor] = 'X';
-
-  for(int i = 0; i<3; i++)
-    cout << closedDoors[i] << " ";
-  cout << endl;
   
-  char decision;
-  cout << "Change doors or stay? (c or s): ";
-  cin >> decision;
-  if(decision == 'c'){
-    playerChoice++;
-    if((playerChoice-1) == removeDoor){
-      playerChoice++;
-    }
-    if(playerChoice > 3){
-      playerChoice = 1;
-    }
-  }
-  for(int i = 0; i<3; i++)
-    cout << doors[i] << " ";
-  cout << endl;
   if((playerChoice-1) == carDoor)
-    cout << "You Win!" << endl;
+    return true;
   else
-    cout << "You Lose :(" << endl;
+    return false;
 }
